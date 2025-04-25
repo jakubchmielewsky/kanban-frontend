1e2fb522b1f8abb2828f96d613209f736dd83397
Jakub Chmielwski <kuba91821@gmail.com>
2025-04-25 23:00:08 +0200

    feat: add synchronisation with session storage to modal store

--- src/shared/stores/useModalStore.ts
+++ src/shared/stores/useModalStore.ts
@@ -1,5 +1,6 @@
 import { create } from "zustand";
 import { ModalType } from "../types/modals";
+import { createJSONStorage, persist } from "zustand/middleware";
 
 interface ModalState {
   current: ModalType | null;
@@ -7,8 +8,16 @@ interface ModalState {
   closeModal: () => void;
 }
 
-export const useModalStore = create<ModalState>((set) => ({
-  current: null,
-  openModal: (modal) => set({ current: modal }),
-  closeModal: () => set({ current: null }),
-}));
+export const useModalStore = create<ModalState>()(
+  persist(
+    (set) => ({
+      current: null,
+      openModal: (modal) => set({ current: modal }),
+      closeModal: () => set({ current: null }),
+    }),
+    {
+      name: "modal-store",
+      storage: createJSONStorage(() => sessionStorage),
+    }
+  )
+);
