4d14764a99625d933669135c6dc0ab3125f38b1b
Jakub Chmielwski <kuba91821@gmail.com>
2025-04-30 23:24:12 +0200

    feat: add basic socket.io event listeners

--- src/features/boards/hooks/useBoardSockets.ts
+++ src/features/boards/hooks/useBoardSockets.ts
@@ -1,5 +1,6 @@
-import { socket } from "../../../lib/socket";
 import { useEffect } from "react";
+import { socket } from "../../../lib/socket";
+import { Task } from "../../../shared/types/task";
 
 export const useBoardSockets = (boardId: string) => {
   useEffect(() => {
@@ -11,4 +12,26 @@ export const useBoardSockets = (boardId: string) => {
       socket.emit("leave_board", boardId);
     };
   }, [boardId]);
+
+  useEffect(() => {
+    const onTaskCreated = (task: Task) => {
+      console.log("Event: task_created", task);
+    };
+    const onTaskUpdated = (task: Task) => {
+      console.log("Event: task_updated", task);
+    };
+    const onTaskDeleted = (taskId: string) => {
+      console.log("Event: task_deleted", taskId);
+    };
+
+    socket.on("task_created", onTaskCreated);
+    socket.on("task_updated", onTaskUpdated);
+    socket.on("task_deleted", onTaskDeleted);
+
+    return () => {
+      socket.off("task_created", onTaskCreated);
+      socket.off("task_updated", onTaskUpdated);
+      socket.off("task_deleted", onTaskDeleted);
+    };
+  }, []);
 };
